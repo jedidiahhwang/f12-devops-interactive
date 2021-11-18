934d90c11faaeb6fb612ae1a5b4155f7ef3d93ba
Jedidiah <jedidiah.hwang.dev@gmail.com>
2021-11-18 11:29:51 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+const students = ["Jeddy"];
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/api/students", (req, res) => {
+    res.status(200).send(students);
+});
+
+const port = process.env.PORT || 4545;
+
+app.listen(port, () => {
+    console.log(`They're taking the Hobbits to ${port}`);
+});
